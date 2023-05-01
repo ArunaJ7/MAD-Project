8f539e7e8a4549a6ea1cd01144786d60555ad03d
IT21183386 <it21183386@my.sliit.lk>
2023-05-01 10:36:34 +0530

    Srvices Update

---
+++ app/src/main/java/com/example/madproject/Srvices_S.kt
@@ -0,0 +1,11 @@
+package com.example.madproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Srvices_S : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_srvices_s)
+    }
+}
